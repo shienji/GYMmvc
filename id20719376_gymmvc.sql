04af1012e1d701e5aad25bd38ab1b8501c9b7cf9
Wilsin <wilsinkarimunwkk@gmail.com>
2023-05-15 20:01:44 +0700

    modified:   id20719376_gymmvc.sql

--- id20719376_gymmvc.sql
+++ id20719376_gymmvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2023 at 02:21 PM
+-- Generation Time: May 15, 2023 at 02:57 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,26 +24,155 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL,
+  `event_nama` varchar(255) DEFAULT NULL,
+  `event_start` datetime DEFAULT NULL,
+  `event_end` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fasilitas`
+--
+
+CREATE TABLE `fasilitas` (
+  `fasilitas_id` int(11) NOT NULL,
+  `fasilitas_nama` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelatih`
+--
+
+CREATE TABLE `pelatih` (
+  `pelatih_id` int(11) NOT NULL,
+  `pelatih_nik` int(11) DEFAULT NULL,
+  `pelatih_role` varchar(255) DEFAULT NULL,
+  `pelatih_nama` varchar(255) DEFAULT NULL,
+  `pelatih_keahlian` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peralatan`
+--
+
+CREATE TABLE `peralatan` (
+  `peralatan_id` int(11) NOT NULL,
+  `fasilitas_id` int(11) DEFAULT NULL,
+  `peralatan_nama` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `role_nama` varchar(255) DEFAULT NULL,
+  `role_harga` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `transaksi_Daftar` datetime DEFAULT NULL,
+  `transaksi_expired` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `user_nama` varchar(255) NOT NULL,
-  `user_password` varchar(255) NOT NULL,
-  `user_role` varchar(255) NOT NULL,
-  `user_nik` int(11) NOT NULL,
-  `user_tgllahir` date NOT NULL,
-  `user_nohp` varchar(255) NOT NULL,
-  `user_email` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_nama` varchar(255) DEFAULT NULL,
+  `user_password` text DEFAULT NULL,
+  `user_role` varchar(255) DEFAULT NULL,
+  `user_nik` int(11) DEFAULT NULL,
+  `user_tgllahir` date DEFAULT NULL,
+  `user_nohp` int(11) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `fasilitas`
+--
+ALTER TABLE `fasilitas`
+  ADD PRIMARY KEY (`fasilitas_id`);
+
+--
+-- Indexes for table `pelatih`
+--
+ALTER TABLE `pelatih`
+  ADD PRIMARY KEY (`pelatih_id`);
+
+--
+-- Indexes for table `peralatan`
+--
+ALTER TABLE `peralatan`
+  ADD PRIMARY KEY (`peralatan_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`);
 
 --
 -- Indexes for table `user`
@@ -54,6 +183,42 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fasilitas`
+--
+ALTER TABLE `fasilitas`
+  MODIFY `fasilitas_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pelatih`
+--
+ALTER TABLE `pelatih`
+  MODIFY `pelatih_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `peralatan`
+--
+ALTER TABLE `peralatan`
+  MODIFY `peralatan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
