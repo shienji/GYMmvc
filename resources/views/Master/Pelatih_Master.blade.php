429108955fd905dda6c13aa5394d9f55d34b342e
kindly <118109002+kindly2424@users.noreply.github.com>
2023-05-26 16:07:32 +0700

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   resources/views/Master/Pelatih_Master.blade.php

--- resources/views/Master/Pelatih_Master.blade.php
+++ resources/views/Master/Pelatih_Master.blade.php
@@ -11,14 +11,14 @@
     <input type="" id="flashpesan" icon="success" value="{{ Session::get('success') }}">
 @endif
 @section('konten')
-<h1 style="text-align: center">Master Role</h1>
+<h1 style="text-align: center">Pelatih Role</h1>
 <br>
     <div class="row">
         <div class="col-lg-5 col-md-12">
             <div class="card card-primary">
                 <div class="card-header border-0">
                     <div class="d-flex justify-content-between">
-                        <h3 class="card-title">Insert New Role</h3>
+                        <h3 class="card-title">Insert New Pelatih</h3>
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                         </div>
