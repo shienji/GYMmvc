5312687230fbea889ad92ad1cc91a4a04e932d17
kindly <118109002+kindly2424@users.noreply.github.com>
2023-05-29 00:47:16 +0700

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   public/dist/js/master_peralatan.js

---
+++ public/dist/js/master_peralatan.js
@@ -0,0 +1,64 @@
+$(function () {
+    setInterval(updateTime,1000);
+   loadListNewMember("#master_peralatan");
+   function updateTime(){
+       var serverOffset=0;
+       timestamp=moment().add(serverOffset,'ms').format('DD-MM-YYYY HH:mm:ss');
+       $('.timestamp').val(timestamp);
+   }
+
+   function loadListNewMember(namanya) {
+       if (namanya) {
+           var nmTabel = $(namanya).DataTable({
+               scrollY: false,
+               scrollX: true,
+               scrollCollapse: true,
+               orderCellsTop: true,
+               fixedHeader: true,
+               processing: true,
+               serverSide: false,
+               searching: true,
+               lengthChange: true,
+               cache: false,
+               autoWidth: true,
+               select: {
+                   style: 'single'
+               },
+               pageLength: 10,
+               lengthMenu: [[10, 25, 50, -1], [10, 25, 50, 'All']],
+               dom: "<'row'<'col-md-2' f><'col-md-6'><'col-md-4'> >" + "<'row'<'col-md-12'rt>> <'row'<'col-md-4'i><'col-md-8'p>>",
+               ajax: {
+                   url: $(namanya).attr('dataLoad'),
+                   dataSrc: ""
+               },
+               columns: [{
+                       "data": "peralatan_id",
+                       "width": "8%"
+                   }, {
+                       "data": "fasilitas_nama",
+                       "width": "15%"
+                   }, {
+                       "data": "peralatan_nama",
+                       "width": "15%"
+                   },{
+                    "data": "peralatan_status",
+                    "width": "15%"
+                     }
+               ],
+                order: [[0, "asc"], [1, "desc"]]
+
+           });
+
+           $(namanya + ' tbody').on('click', 'tr', function () {
+               data = nmTabel.row(this).data();
+               exDate = moment(new Date(), "DD-MM-YYYY").add(1, 'Month').format("YYYY-MM-DD");
+               $('#peralatan_id').val(data.peralatan_id);
+               $('#peralatan_id2').val(data.peralatan_id);
+               $('#fasilitas_nama').val(data.fasilitas_nama);
+               $('#nama').val(data.peralatan_nama);
+
+           });
+       }
+   }
+
+});
